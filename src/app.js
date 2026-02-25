937b0ff851dc4a2e294f36f446368d1c14f0df67
jhunadev <jhunalbisdeyanquintero@gmail.com>
2026-02-25 23:29:23 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  
+  let rdmWho = who[Math.floor(Math.random() * who.length)];
+  let rdmAction = action[Math.floor(Math.random() * action.length)];
+  let rdmWhat = what[Math.floor(Math.random() * what.length)];
+  let rdmWhen = when[Math.floor(Math.random() * when.length)];
+
+  
+  let excuse = rdmWho + ' ' + rdmAction + ' ' + rdmWhat + ' ' + rdmWhen;
+
+  
+  document.querySelector('#excuse').innerHTML = excuse;
+
+ 
+  console.log("Tu excusa es: " + excuse);
 };
